1cd5f60a2c8aef46ec6398e37d8814f93b835757
reo <chaehunjeong>
2017-08-30 23:10:33 +0900

    initlal commit

---
+++ src/main/java/net/reo/MyReoApplication.java
@@ -0,0 +1,12 @@
+package net.reo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReoApplication.class, args);
+	}
+}
